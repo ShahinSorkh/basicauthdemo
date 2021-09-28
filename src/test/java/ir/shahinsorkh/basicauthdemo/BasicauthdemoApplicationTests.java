24c20b0aa62b004ed237b663a4f18fedae989a96
Shahin Sorkh <sorkh.shahin@protonmail.com>
2021-09-28 21:46:46 +0330

    init

---
+++ src/test/java/ir/shahinsorkh/basicauthdemo/BasicauthdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.shahinsorkh.basicauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicauthdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
